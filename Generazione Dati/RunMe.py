27049ebfbd5cc5cdc00631a475c672b448eb7d99
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-06-20 16:36:10 +0200

    precedures created but not perfect

--- Generazione Dati/RunMe.py
+++ Generazione Dati/RunMe.py
@@ -1,9 +1,9 @@
 import json
 import random
 
-expA = 5
+expA = 3
 
-expB = 6
+expB = 4
 
 N_POST = 10**expA 
 
@@ -51,10 +51,8 @@ def choose_option():
     return val 
 
 def referencing_PC():
-    for i in range(10000):
+    for i in range(N_POST):
         POST.append({"AK" : i, "A1" : i%N_ATT1, "A2" : i%N_ATT2, "A3" : i%N_ATT3, "A4" : i%N_ATT1, "A5" : i%N_ATT2, "A6" : i%N_ATT3, "A7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
-        #file.write(json.dumps({"AK" : i, "A1" : i%N_ATT1, "A2" : i%N_ATT2, "A3" : i%N_ATT3, "A4" : i%N_ATT1, "A5" : i%N_ATT2, "A6" : i%N_ATT3, "A7" : "cacca"}))
-        #lists = {"id" : 1 , "POST" : POST}
     lists = {"id" : 1 , "Post" : POST}
     file_p.write(json.dumps(lists))
     for i in range(N_COMMENT):
@@ -64,16 +62,41 @@ def referencing_PC():
 
 
 def referencing_CP():
-    return null
+    for i in range(N_COMMENT):
+        COMMENT.append({"BK" : i, "B1" : i%N_ATT1, "B2" : i%N_ATT2, "B3" : i%N_ATT3, "B4" : i%N_ATT1, "B5" : i%N_ATT2, "B6" : i%N_ATT3, "B7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    lists = {"id" : 2, "Comment" : COMMENT}
+    file_c.write(json.dumps(lists))
+    for i in range(N_POST):
+        POST.append({"AK" : i, "BK" : random.randint(0, N_COMMENT - 1) ,"A1" : i%N_ATT1, "A2" : i%N_ATT2, "A3" : i%N_ATT3, "A4" : i%N_ATT1, "A5" : i%N_ATT2, "A6" : i%N_ATT3, "A7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    lists = {"id" : 1 , "Post" : POST}
+    file_p.write(json.dumps(lists))
+ 
+
 
 def embedding_PC():
-    return null
+    for i in range(N_COMMENT):
+        COMMENT.append({"BK" : i, "B1" : i%N_ATT1, "B2" : i%N_ATT2, "B3" : i%N_ATT3, "B4" : i%N_ATT1, "B5" : i%N_ATT2, "B6" : i%N_ATT3, "B7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    for i in range(N_POST):
+        POST.append({"AK" : i, "A1" : i%N_ATT1, "A2" : i%N_ATT2, "A3" : i%N_ATT3, "A4" : i%N_ATT1, "A5" : i%N_ATT2, "A6" : i%N_ATT3, "A7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    ## NON VA BENE COSI i post non sono relativi ai commenti....
+    POST.append(COMMENT)
+    lists = {"Post with comment" : POST}
+    file_s.write(json.dumps(lists))
 
 def embedding_CP():
-    return null
+    for i in range(N_COMMENT):
+        COMMENT.append({"BK" : i, "B1" : i%N_ATT1, "B2" : i%N_ATT2, "B3" : i%N_ATT3, "B4" : i%N_ATT1, "B5" : i%N_ATT2, "B6" : i%N_ATT3, "B7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    for i in range(N_POST):
+        POST.append({"AK" : i, "A1" : i%N_ATT1, "A2" : i%N_ATT2, "A3" : i%N_ATT3, "A4" : i%N_ATT1, "A5" : i%N_ATT2, "A6" : i%N_ATT3, "A7" : "In vel mauris felis. Cras consectetur nisi quis diam molestie dapibus. Donec faucibus non urna et blandit. In ac gravida turpis, suscipit pulvinar arcu. Aenean eros purus, vehicula quis facilisis sit amet, luctus non est. Vestibulum at sapien odio. Nulla at scelerisque justo, vel tristique magna. Nullam semper porta lacinia. Integer neque odio, iaculis quis aliquam sit amet, ultrices quis libero. Vestibulum non feugiat lectus. Aliquam erat volutpat. Proin dignissim sollicitudin pretium. Donec laoreet, nisi a feugiat tristique, enim felis aliquam dui, sed eleifend quam massa eu lorem. Proin pulvinar nunc sit amet eros consequat tristique. Pellentesque sed nisl elit.Fusce elementum fermentum diam, quis tristique turpis vulputate eu. Curabitur laoreet odio enim, at faucibus lacus sodales quis. Proin porta justo vel vestibulum lobortis. Nulla eros turpis, accumsan id efficitur quis, scelerisque eu ex. In eleifend nisi at purus condimentum, nec semper urna sodales. Suspendisse interdum dui."})
+    ## NON VA BENE COSI i commmenti non sono relativi ai post....
+    COMMENT.append(POST)
+    lists = {"Comment with post" : COMMENT}
+    file_s.write(json.dumps(lists))
+    
 
 file_p = open("post.json", "w")
 file_c = open("comment.json", "w")
+file_s = open("commentAndPost.json", "w")
 
 if __name__ == "__main__":
     choosenOne = choose_option()
