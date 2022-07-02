7212759a3e9e0b2191b9aaa1eb9f277e18f27df2
Matteo Santoro <megadigimatt@gmail.com>
2022-07-02 16:38:10 +0200

    queries of selection are now working

--- CouchDB/container/querySelector.py
+++ CouchDB/container/querySelector.py
@@ -1,3 +1,5 @@
+import requests
+import json
 import os
 
 ind_a= ["A1",  "A2",  "A3",  "A4",  "A5", "A6"]
@@ -11,8 +13,9 @@ collection_titles = [ l.lower() for l in collections] # lower case altrimenti co
 print("On which collection should i run the query")
 n = int(input("0)embedding_A_in_B.json\n1)embedding_B_in_A.json\n2)referencing_A_in_B.json\n3)referencing_B_in_A.json\n\n"))
 
-collection = collection_titles[n]
+collection = collection_titles[n][:-5]
 
+print(collection)
 if n%2 == 1:
     print("On which index should i run the query")
     i = input("0)A1 \n1)A2 \n2)A3 \n3)A4 \n4)A5 \n5)A6\n\n")
@@ -22,4 +25,23 @@ else:
     i = input("0)B1 \n1)B2 \n2)B3 \n3)B4 \n4)B5 \n5)B6\n\n")
     ind = ind_b[int(i)]
 
-# TODO queries
+
+val = input("On which value?\n\n")
+
+url = "http://admin:admin@127.0.0.1:5984/" + collection +"/_find"
+
+payload = json.dumps({
+  "selector": {
+    ind : int(val)
+  },
+  "execution_stats": True
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+print(payload)
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
--- CouchDB/importExport.py
+++ CouchDB/importExport.py
@@ -4,49 +4,50 @@ from tqdm import tqdm
 
 couch = couchdb.Server("http://admin:admin@127.0.0.1:5984")
 
-db = couch.create("a")
-
-with open("A.json") as jsonfile:
-        for row in tqdm(jsonfile):
-            db_entry = json.loads(row) 
-            db.save(db_entry)
-
-db = couch.create("b")
-
-with open("B.json") as jsonfile:
-        for row in tqdm(jsonfile):
-            db_entry = json.loads(row) 
-            db.save(db_entry)
+#db = couch.create("a")
+#
+#with open("A.json") as jsonfile:
+#        for row in tqdm(jsonfile):
+#            db_entry = json.loads(row) 
+#            db.save(db_entry)
+#
+#db = couch.create("b")
+#
+#with open("B.json") as jsonfile:
+#        for row in tqdm(jsonfile):
+#            db_entry = json.loads(row) 
+#            db.save(db_entry)
 
 try:
     db = couch.create("referencing_a_in_b")
-    with open("referencing_A_in_B.json") as jsonfile:
+    with open("../dataJSON/referencing_A_in_B.json") as jsonfile:
         for row in tqdm(jsonfile):
             db_entry = json.loads(row) 
             db.save(db_entry)
-
+ 
     db = couch.create("referencing_b_in_a")
-    with open("referencing_B_in_A.json") as jsonfile:
+    with open("../dataJSON/referencing_B_in_A.json") as jsonfile:
         for row in tqdm(jsonfile):
             db_entry = json.loads(row) 
             db.save(db_entry)
 except IOError:
     print("No documents found")
-
+ 
 try:
     db = couch.create("embedding_a_in_b")
-    with open("embedding_A_in_B.json") as jsonfile:
+    with open("../dataJSON/embedding_A_in_B.json") as jsonfile:
         for row in tqdm(jsonfile):
             db_entry = json.loads(row) 
             db.save(db_entry)
 except IOError:
     print("No redunant document found")
-
+ 
 try:
     db = couch.create("embedding_b_in_a")
-    with open("embedding_B_in_A.json") as jsonfile:
+    with open("../dataJSON/embedding_B_in_A.json") as jsonfile:
         for row in tqdm(jsonfile):
             db_entry = json.loads(row) 
             db.save(db_entry)
 except IOError:
     print("No redunant document found")
+ 
