b6c1b3cd015377ba7766614d88f3be1f532564c1
Matteo Santoro <megadigimatt@gmail.com>
2022-07-02 14:42:06 +0200

    finally i can produce an entire Json as result of the queries

--- MongoDB/container/querySelector.sh
+++ MongoDB/container/querySelector.sh
@@ -32,5 +32,5 @@ echo -e "printjson(db.getCollection('$col').explain('queryPlanner').aggregate([ 
 echo -e "use('tirocinio')"												>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js" 
 echo -e "printjson(db.getCollection('$col').explain('executionStats').aggregate([ { \$match: { \"$ind\" : $v}}]))" 	>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
 
-docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_query_plan.js" >> "./result/on_"${col}"ind_"${ind}"_query_plan_Result.json"
-docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_exec_stats.js" >> "./result/on_"${col}"ind_"${ind}"_exec_stats_Result.json"
+docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_query_plan.js" | sed 1,25d >> "./result/on_"${col}"ind_"${ind}"_query_plan_Result.json"
+docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_exec_stats.js" | sed 1,25d >> "./result/on_"${col}"ind_"${ind}"_exec_stats_Result.json"
