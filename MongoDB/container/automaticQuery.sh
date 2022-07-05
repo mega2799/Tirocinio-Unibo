ca38b81266feafbc4ad3dfd1de8d0d867cab8d1a
Matteo Santoro <megadigimatt@gmail.com>
2022-07-05 21:29:29 +0200

    no more duplicates, json parse works

--- MongoDB/container/automaticQuery.sh
+++ MongoDB/container/automaticQuery.sh
@@ -13,12 +13,10 @@ do
 	for ind in "A1"  "A2" "A3"  "A4"  "A5" "A6"
 	do
 		echo -e "use('tirocinio')"												>> "queries/on_"${col}"ind_"${ind}"_query_plan.js" 
-		echo -e "result = db.getCollection('$col').explain('queryPlanner').aggregate([ { \$match: { \"$ind\" : $v}}])"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
-		echo -e "print(EJSON.stringify(result, null, \"  \")"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
+		echo -e "print(EJSON.stringify(db.getCollection('$col').explain('queryPlanner').aggregate([ { \$match: { \"$ind\" : $v}}]), null, \"  \")"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
 		
 		echo -e "use('tirocinio')"												>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js" 
-		echo -e "result = db.getCollection('$col').explain('executionStats').aggregate([ { \$match: { \"$ind\" : $v}}])" 	>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
-		echo -e "print(EJSON.stringify(result, null, \" \"))" >> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
+		echo -e "print(EJSON.stringify(db.getCollection('$col').explain('executionStats').aggregate([ { \$match: { \"$ind\" : $v}}]), null, \" \"))" >> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
 
 	
 		docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_query_plan.js" | sed 1,25d >> "./result/on_"${col}"ind_"${ind}"_query_plan_Result.json"
@@ -33,12 +31,10 @@ do
 	for ind in "B1"  "B2" "B3"  "B4"  "B5" "B6"
 	do
 		echo -e "use('tirocinio')"												>> "queries/on_"${col}"ind_"${ind}"_query_plan.js" 
-		echo -e "result = db.getCollection('$col').explain('queryPlanner').aggregate([ { \$match: { \"$ind\" : $v}}])"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
-		echo -e "print(EJSON.stringify(result, null, \"  \")"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
+		echo -e "print(EJSON.stringify(db.getCollection('$col').explain('queryPlanner').aggregate([ { \$match: { \"$ind\" : $v}}]), null, \"  \")"  	>> "queries/on_"${col}"ind_"${ind}"_query_plan.js"
 		
 		echo -e "use('tirocinio')"												>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js" 
-		echo -e "result = db.getCollection('$col').explain('executionStats').aggregate([ { \$match: { \"$ind\" : $v}}])" 	>> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
-		echo -e "print(EJSON.stringify(result, null, \" \"))" >> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
+		echo -e "print(EJSON.stringify(db.getCollection('$col').explain('executionStats').aggregate([ { \$match: { \"$ind\" : $v}}]), null, \" \"))" >> "queries/on_"${col}"ind_"${ind}"_exec_stats.js"	
 
 		
 		docker exec -it mongodb mongosh -u root -p pass12345 --authenticationDatabase admin -f "/home/queries/on_"${col}"ind_"${ind}"_query_plan.js" | sed 1,25d >> "./result/on_"${col}"ind_"${ind}"_query_plan_Result.json"
