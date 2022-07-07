f41b0e96c6533f62fc237d5b7c7d242ce6168879
Matteo Santoro <megadigimatt@gmail.com>
2022-07-07 11:37:52 +0200

    cant run queries on mongo idk why......

--- MongoDB/container/operatorMongo.py
+++ MongoDB/container/operatorMongo.py
@@ -1,5 +1,6 @@
 import pymongo
 import json
+import time
 
 
 ind_a= ["A1",  "A2",  "A3",  "A4",  "A5", "A6"]
@@ -16,7 +17,7 @@ def simple_aggregate(collection ,query):
     {
         'aggregate': collection, 
         'pipeline': query, 
-        'cursor': {}
+        'cursor': {}, 
     })
 
 def explain_plan(collection, query):
@@ -47,36 +48,56 @@ def exec_cost(collection, query):
     ) 
 
 if __name__ == "__main__":
-    print("On which collection should i run the query")
-    n = int(input("0)embedding_A_in_B.json\n1)embedding_B_in_A.json\n2)referencing_A_in_B.json\n3)referencing_B_in_A.json\n\n"))
 
-    collection = collections[n]
-
-    if n%2 == 1:
-        print("On which index should i run the query")
-        i = input("0)A1 \n1)A2 \n2)A3 \n3)A4 \n4)A5 \n5)A6\n\n")
+    print("On which index should i run the query")
+    i = input("0)A1 \n1)A2 \n2)A3 \n3)A4 \n4)A5 \n5)A6   None\n\n")
+    if i != "None":
         ind = ind_a[int(i)]
     else:
         print("On which index should i run the query")
         i = input("0)B1 \n1)B2 \n2)B3 \n3)B4 \n4)B5 \n5)B6\n\n")
         ind = ind_b[int(i)]
     
+    print(ind)
     val = input("On which value?\n\n")
+    
+    if ind in ind_a:
+        query = (
+            [
+                {
+                    '$match' : { ind : val}
+                }
+            ]
+        )
 
-    query = (
-        [
-            {
-                '$match' : { ind : val}
-            }
-        ]
-    )
+        for collection in ["embedding_B_in_A",  "referencing_B_in_A"]:
+            start_time = time.time()
+            res = simple_aggregate(collection, query)
+            file = open("result/" + collection + "@" + ind + "exec_stats.json", "w") 
+            print("--- %s seconds ---" % (time.time() - start_time))
+            file.write(json.dumps(res, indent=4))
+
+        query = (
+            [
+                {
+
+                }
+            ]
+        )
+        for collection in ["embedding_A_in_B",  "referencing_B_in_A"]:
+            start_time = time.time()
+            res = simple_aggregate(collection, query)
+            file = open("result/" + collection + "@" + ind + "exec_stats.json", "w") 
+            print("--- %s seconds ---" % (time.time() - start_time))
+            file.write(json.dumps(res, indent=4))
 
 
-    print(simple_aggregate(collection, query))
 
-    file = open("result/" + collection + "@" + ind + "exec_stats.json", "w") 
+
+    exit(0)
 
     res = exec_cost(collection, query)
+
 
     print(res)
 
@@ -89,7 +110,11 @@ if __name__ == "__main__":
 
     file = open("result/" + collection + "@" + ind + "query_plan.json", "w") 
 
+    start_time = time.time()
+
     res = explain_plan(collection, query)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     file.write(json.dumps(res, indent=4))
 
