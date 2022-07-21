8ed1b430152624cf44a0ea89f4a981c11931bbeb
Matteo Santoro <megadigimatt@gmail.com>
2022-07-21 16:16:36 +0200

    better queries

--- MongoDB/scripts/makeIndex.js
+++ MongoDB/scripts/makeIndex.js
@@ -4,13 +4,13 @@ use('tirocinio')
 db.getCollection('A').createIndex({'AK': 1});
 db.getCollection('B').createIndex({'BK': 1});
 
-db.getCollection('A').createIndex({'A3': 1});               
 db.getCollection('A').createIndex({'A4': 1});               
 db.getCollection('A').createIndex({'A5': 1});               
+db.getCollection('A').createIndex({'A6': 1});               
 
-db.getCollection('B').createIndex({'B3': 1});               
 db.getCollection('B').createIndex({'B4': 1});               
 db.getCollection('B').createIndex({'B5': 1});
+db.getCollection('B').createIndex({'B6': 1});               
 
 db.getCollection('embedding_A_in_B').createIndex({'B4': 1});
 db.getCollection('embedding_A_in_B').createIndex({'B5': 1});
@@ -21,6 +21,9 @@ db.getCollection('embedding_B_in_A').createIndex({'A6': 1});
 db.getCollection('referencing_A_in_B').createIndex({'B4': 1});
 db.getCollection('referencing_A_in_B').createIndex({'B5': 1});
 db.getCollection('referencing_A_in_B').createIndex({'B6': 1});
+db.getCollection('referencing_A_in_B').createIndex({'AK': 1});               
+
 db.getCollection('referencing_B_in_A').createIndex({'A4': 1});
 db.getCollection('referencing_B_in_A').createIndex({'A5': 1});
 db.getCollection('referencing_B_in_A').createIndex({'A6': 1});
+db.getCollection('referencing_B_in_A').createIndex({'BK': 1});               
