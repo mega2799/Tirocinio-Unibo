416e882f15b8ce58ba3e62ca8de7d0d826e38110
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-07-25 21:56:38 +0200

    indx

--- CouchDB/container/makeIndexes.py
+++ CouchDB/container/makeIndexes.py
@@ -10,6 +10,43 @@ collections_b = ['embedding_a_in_b',  'referencing_a_in_b']
 ind_a= ["A4",  "A5", "A6"]
 ind_b= ["B4",  "B5", "B6"]
 
+url = "http://admin:admin@127.0.0.1:5984/a/_index?partitioned=true"
+payload = json.dumps({
+  "index": {
+    "fields": [
+      "AK"
+    ]
+  },
+  "name": "AK" + "-index",
+  "type": "json"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response)
+
+
+url = "http://admin:admin@127.0.0.1:5984/b/_index?partitioned=true"
+payload = json.dumps({
+  "index": {
+    "fields": [
+      "BK"
+    ]
+  },
+  "name": "AK" + "-index",
+  "type": "json"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response)
+exit(0)
+
 for collection in collections_a:
     url = "http://admin:admin@127.0.0.1:5984/" + collection + "/_index?partitioned=true"
     for ind  in ind_a:
