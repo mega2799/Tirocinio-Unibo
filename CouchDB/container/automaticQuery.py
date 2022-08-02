452dc95327ae2b41555ddf9911029e6510a29758
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-08-02 21:13:55 +0200

    added a0join

--- CouchDB/container/automaticQuery.py
+++ CouchDB/container/automaticQuery.py
@@ -26,37 +26,38 @@ worksheet.write_string('C1', 'embedding_B_in_A', bold)
 worksheet.write_string('D1', 'referencing_A_in_B', bold)
 worksheet.write_string('E1', 'referencing_B_in_A', bold)
 
-worksheet.write_string('A2', 'A1', bold)
-worksheet.write_string('A3', 'A2', bold)
-worksheet.write_string('A4', 'A3', bold)
-worksheet.write_string('A5', 'A4', bold)
-worksheet.write_string('A6', 'A5', bold)
-worksheet.write_string('A7', 'A6', bold)
-worksheet.write_string('A8', 'B1', bold)
-worksheet.write_string('A9', 'B2', bold)
-worksheet.write_string('A10', 'B3', bold)
-worksheet.write_string('A11', 'B4', bold)
-worksheet.write_string('A12', 'B5', bold)
-worksheet.write_string('A13', 'B6', bold)
+worksheet.write_string('A2', 'A0', bold)
+worksheet.write_string('A3', 'A1', bold)
+worksheet.write_string('A4', 'A2', bold)
+worksheet.write_string('A5', 'A3', bold)
+worksheet.write_string('A6', 'A4', bold)
+worksheet.write_string('A7', 'A5', bold)
+worksheet.write_string('A8', 'A6', bold)
+worksheet.write_string('A9', 'B0', bold)
+worksheet.write_string('A10', 'B1', bold)
+worksheet.write_string('A11', 'B2', bold)
+worksheet.write_string('A12', 'B3', bold)
+worksheet.write_string('A13', 'B4', bold)
+worksheet.write_string('A14', 'B5', bold)
+worksheet.write_string('A15', 'B6', bold)
+worksheet.write_string('A16' , 'A0j', bold)
+worksheet.write_string('A17' , 'A1j', bold)
+worksheet.write_string('A18' , 'A2j', bold)
+worksheet.write_string('A19' , 'A3j', bold)
+worksheet.write_string('A20' , 'A4j', bold)
+worksheet.write_string('A21' , 'A5j', bold)
+worksheet.write_string('A22' , 'A6j', bold)
+worksheet.write_string('A23' , 'B0j', bold)
+worksheet.write_string('A24' , 'B1j', bold)
+worksheet.write_string('A25' , 'B2j', bold)
+worksheet.write_string('A26' , 'B3j', bold)
+worksheet.write_string('A27' , 'B4j', bold)
+worksheet.write_string('A28' , 'B5j', bold)
+worksheet.write_string('A29' , 'B6j', bold)
 
-worksheet.write_string('A14' , 'A1j', bold)
-worksheet.write_string('A15' , 'A2j', bold)
-worksheet.write_string('A16' , 'A3j', bold)
-worksheet.write_string('A17' , 'A4j', bold)
-worksheet.write_string('A18' , 'A5j', bold)
-worksheet.write_string('A19' , 'A6j', bold)
-worksheet.write_string('A20' , 'B1j', bold)
-worksheet.write_string('A21' , 'B2j', bold)
-worksheet.write_string('A22' , 'B3j', bold)
-worksheet.write_string('A23' , 'B4j', bold)
-worksheet.write_string('A24' , 'B5j', bold)
-worksheet.write_string('A25' , 'B6j', bold)
 
 get_rowNum = {'embedding_a_in_b' : 0,'embedding_b_in_a' : 1,'referencing_b_in_a' : 2,'referencing_a_in_b' : 3}
-get_colNum = {
-    'A1' : 0,'A2' : 1,'A3' : 2,'A4' : 3, 'A5' : 4, 'A6' : 5, 'B1' : 6,'B2' : 7,'B3' : 8,'B4' : 9, 'B5' : 10, 'B6' : 11, 
-    'A1join' : 12,'A2join' : 13,'A3join' : 14,'A4join' : 15, 'A5join' : 16, 'A6join' : 17, 'B1join' : 18,'B2join' : 19,'B3join' : 20,'B4join' : 21, 'B5join' : 22, 'B6join' : 23
-}
+get_colNum = {'A0' : 0,'A1' : 1,'A2' : 2,'A3' : 3,'A4' : 4,'A5' : 5,'A6' : 6,'B0' : 6,'B1' : 7,'B2' : 8,'B3' : 9,'B4' : 10,'B5' : 11,'B6' : 12,'A0join' : 12,'A1join' : 13,'A2join' : 14,'A3join' : 15,'A4join' : 16,'A5join' : 17,'A6join' : 18,'B0join' : 18,'B1join' : 19,'B2join' : 20,'B3join' : 21,'B4join' : 22,'B5join' : 23,'B6join' : 24}
 
 def queryRun(collection, ind, payload):
   url = "http://admin:admin@127.0.0.1:5984/" + collection +"/_find"
@@ -69,98 +70,72 @@ def queryRun(collection, ind, payload):
   response = requests.request("POST", url, headers=headers, data=payload)
 
   resp = json.loads(response.text)
-  check(resp)
+  check(resp, collection, ind)
   time += resp["execution_stats"]['execution_time_ms']
   # worksheet.write(get_rowNum[collection] + 1, get_colNum[ind] + 1, time)
   print("sel time: " )
   print(str(time) + " ms") 
   return time
 
-def check(p):
+def check(p, col, ind):
   if p["execution_stats"]['results_returned'] == 0:
+    print(col)
+    print(ind)
+    print(p)
     print("ERRROR")
     exit(1)
 
 def simpleSelection(collection, ind, value):
   url = "http://admin:admin@127.0.0.1:5984/" + collection +"/_find"
-
   time = 0
-
   payload = json.dumps({
     "selector": {
       ind : int(val)
     },
     "execution_stats": True
   })
-
   headers = {
     'Content-Type': 'application/json'
   }
   response = requests.request("POST", url, headers=headers, data=payload)
   resp = json.loads(response.text)
-  check(resp)
+  check(resp, collection, ind)
   time += resp["execution_stats"]['execution_time_ms']
   # worksheet.write(get_rowNum[collection] + 1, get_colNum[ind] + 1, time)
   print("sel time: " )
   print(str(time) + " ms") 
   return time
 
-def joinQuery(to_col, from_col, ind, value):
-  url = "http://admin:admin@127.0.0.1:5984/" + from_col +"/_find"
 
+def simpleSelectionResp(collection, ind, value):
+  url = "http://admin:admin@127.0.0.1:5984/" + collection +"/_find"
+  time = 0
   payload = json.dumps({
-   "selector": {
+    "selector": {
       ind : int(val)
-   },
-   "fields": [
-      "AK"
-     # ,                da qui prendo solo le chiavi del database esterno per poter calcolare i tempi magari sarebbe da prendere l intero docuemnto
-     # "A1",
-     # "A2",
-     # "A3",
-     # "A4",
-     # "A5",
-     # "A6",
-     # "A7",
-   ]
-   ,
+    },
     "execution_stats": True
-    })
-
+  })
   headers = {
     'Content-Type': 'application/json'
   }
-
-  time = 0
   response = requests.request("POST", url, headers=headers, data=payload)
-
   resp = json.loads(response.text)
+  check(resp, collection, ind)
   time += resp["execution_stats"]['execution_time_ms']
-  foreign_keys = [list(x.values())[0] for x in resp["docs"]]
-  url = "http://admin:admin@127.0.0.1:5984/" + to_col +"/_find"
-  for key in foreign_keys:
-    payload = json.dumps({
-      "selector": {
-        "_id" : key
-      },
-      "execution_stats": True
-    })
-    response = requests.request("POST", url, headers=headers, data=payload)
-    resp = json.loads(response.text)
-    check(resp)
-    time += resp["execution_stats"]['execution_time_ms']
-  worksheet.write(get_rowNum[to_col] + 1, get_colNum[ind] + 1, time)
-  print("join time: " )
+  # worksheet.write(get_rowNum[collection] + 1, get_colNum[ind] + 1, time)
+  print("sel time: " )
   print(str(time) + " ms") 
-
-
-
+  return time, resp
 
 
 
 def refBA():
   collection = "referencing_b_in_a"
   print("i'm in " + collection)
+  # key 
+  t = simpleSelection(collection, "AK", val)
+  worksheet.write( get_colNum["A0"] + 1, get_rowNum[collection] + 1,t)
   for ind in ind_a:
       # select A.*
       # from A
@@ -195,7 +170,7 @@ def refBA():
     time = 0
     response = requests.request("POST", url, headers=headers, data=payload)
     resp = json.loads(response.text)
-    check(resp)
+    check(resp, collection, ind)
     time += resp["execution_stats"]['execution_time_ms']
     # print(resp["docs"])
     foreign_keys = [list(x.values())[10] for x in resp["docs"]]
@@ -210,7 +185,7 @@ def refBA():
           })
           response = requests.request("POST", url, headers=headers, data=payload)
           resp = json.loads(response.text)
-          check(resp)
+          check(resp, collection, ind)
           time += resp["execution_stats"]['execution_time_ms']
     worksheet.write( get_colNum[(ind + "join")] + 1,get_rowNum[collection] + 1, time)
     print("join time: " )
@@ -250,7 +225,7 @@ def refBA():
     time = 0
     response = requests.request("POST", url, headers=headers, data=payload)
     resp = json.loads(response.text)
-    check(resp)
+    check(resp, collection, ind)
     time += resp["execution_stats"]['execution_time_ms']
     foreign_keys = [str(list(x.values())[2]) for x in resp["docs"]]
     url = "http://admin:admin@127.0.0.1:5984/referencing_a_in_b/_find"
@@ -262,7 +237,7 @@ def refBA():
         "execution_stats": True
       })
       response = requests.request("POST", url, headers=headers, data=payload)
-      check(resp)
+      check(resp, collection, ind)
       resp = json.loads(response.text)
       time += resp["execution_stats"]['execution_time_ms']
     worksheet.write( get_colNum[(ind + "join")] + 1,get_rowNum[collection] + 1, time)
@@ -272,6 +247,14 @@ def refBA():
 def refAB():
   collection = "referencing_a_in_b"
   print("i'm in " + collection)
+  # key 
+  t = simpleSelection(collection, "BK", val)
+  worksheet.write( get_colNum["B0"] + 1, get_rowNum[collection] + 1,t)
+  # key join Aj0
+  t,resp = simpleSelectionResp("a", "AK", val)
+  fk = [str(list(x.values())[2]) for x in resp["docs"]]
+  t += simpleSelection(collection, "AK", int(fk[0]))[0]
+  worksheet.write( get_colNum["A0join"] + 1, get_rowNum[collection] + 1,t)
   for ind in ind_a:
       # select A.*
       # from A
@@ -306,7 +289,7 @@ def refAB():
     time = 0
     response = requests.request("POST", url, headers=headers, data=payload)
     resp = json.loads(response.text)
-    check(resp)
+    check(resp, collection, ind)
     time += resp["execution_stats"]['execution_time_ms']
 ##################### TESTARE ##################################################
     foreign_keys = [list(x.values())[2] for x in resp["docs"]]
@@ -320,7 +303,7 @@ def refAB():
       })
       response = requests.request("POST", url, headers=headers, data=payload)
       resp = json.loads(response.text)
-      check(resp)
+      check(resp, collection, ind)
       time += resp["execution_stats"]['execution_time_ms']
     worksheet.write( get_colNum[(ind + "join")] + 1,get_rowNum[collection] + 1, time)
     print("join time: " )
@@ -349,7 +332,7 @@ def refAB():
     time = 0
     response = requests.request("POST", url, headers=headers, data=payload)
     resp = json.loads(response.text)
-    check(resp)
+    check(resp, collection, ind)
     time += resp["execution_stats"]['execution_time_ms']
   ### qui dovrei prendere le chiavi di AK e per ognuna selezionare il doc di A
     foreign_keys = [list(x.values())[3] for x in resp["docs"]]
@@ -357,13 +340,13 @@ def refAB():
     for key in foreign_keys:
       payload = json.dumps({
         "selector": {
-          "_id" : str(key)
+          "AK" : key
         },
         "execution_stats": True
       })
       response = requests.request("POST", url, headers=headers, data=payload)
       resp = json.loads(response.text)
-      check(resp)
+      check(resp, collection, ind)
       time += resp["execution_stats"]['execution_time_ms']
     worksheet.write( get_colNum[(ind + "join")] + 1,get_rowNum[collection] + 1, time)
     print("join time: " )
@@ -374,10 +357,14 @@ if __name__ == "__main__":
 
   val = "8"
 
-  refBA()
+  # refBA()
 
   refAB()
+
   collection = "embedding_b_in_a"
+  # key 
+  t = simpleSelection(collection, "AK", val)
+  worksheet.write( get_colNum["A0"] + 1, get_rowNum[collection] + 1,t)
   for ind in ind_a:
       # select A.*
       # from A
@@ -445,6 +432,10 @@ if __name__ == "__main__":
     worksheet.write( get_colNum[(ind + "join")] + 1,get_rowNum[collection] + 1, t)
 
   collection = "embedding_a_in_b"
+  # NON PUOI ANDARE !!!!!! 
+  # key 
+  t = simpleSelection(collection, "BK", val)
+  worksheet.write( get_colNum["B0"] + 1, get_rowNum[collection] + 1,t)
   for ind in ind_a:
       # select A.*
       # from A
